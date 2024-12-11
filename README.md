02bf470413dc1174b351ff100dcebe5fef7c1bd7
Celia Recio <celia.recio@yahoo.es>
2024-12-11 19:14:01 +0100

    update exercise

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # promo-c-pt-module-2-pair-4-ada-tasks
+
+En este repositorio vamos a crear el ejercicio llamado "Adatasks", donde se puede ver cómo aplicamos nuestros conocimientos de Javascript.
+
+La estructura en carpetas se compone de una carpeta contenedora con un archivo HTML, un archivo CSS y otro JS. 
+
---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const addTasks = document.querySelector ('js_addtasks');
+const addBtn = document.querySelector ('js_addBtn');
+const addInput = document.querySelector ('js_addInput');
+const inputList = document.querySelector ('js_inputList');
+const tasksList = document.querySelector ('js_taskList');
+const filterBtn = document.querySelector ('js_filterBtn');
+
+const tasks = [
+    { name: "Recoger setas en el campo", completed: true, id: 1 },
+    { name: "Comprar pilas", completed: true, id: 2 },
+    { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+    {
+      name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+      completed: false,
+      id: 4,
+    },
+  ];
+
+addBtn.addEventListener( 'click', (ev) => {
+    ev.preventdefault();
+    inputList.innerHTML += `
+    tasks
+    `
+
+
+})
+
+
+
+
+
+
+
