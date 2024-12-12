82725f8c2f30d676b11d067e5cd81dfd2a0eebe8
biancadragan <andreeabiancadragan@gmail.com>
2024-12-12 21:32:06 +0100

    Add newTask Event Listener and other small changes in js file

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 'use strict';
 
-const addTasks = document.querySelector ('js_addtasks');
-const addBtn = document.querySelector ('js_addBtn');
-const addInput = document.querySelector ('js_addInput');
-const inputList = document.querySelector ('js_inputList');
-const tasksList = document.querySelector ('js_taskList');
-const filterBtn = document.querySelector ('js_filterBtn');
+const addTasks = document.querySelector('.js_addtasks');
+const addBtn = document.querySelector('.js_addBtn');
+const addInput = document.querySelector('.js_addInput');
+const inputList = document.querySelector('.js_inputList');
+const tasksList = document.querySelector('.js_taskList');
+const filterBtn = document.querySelector('.js_filterBtn');
 
 const tasks = [
     { name: "Recoger setas en el campo", completed: true, id: 1 },
     { name: "Comprar pilas", completed: true, id: 2 },
     { name: "Poner una lavadora de blancos", completed: true, id: 3 },
     {
-      name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-      completed: false,
-      id: 4,
+        name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+        completed: false,
+        id: 4,
     },
-  ];
+];
 
-addBtn.addEventListener( 'click', (ev) => {
+addBtn.addEventListener('click', (ev) => {
     ev.preventdefault();
-    inputList.innerHTML += `
-    tasks
-    `
+    const newTask = addInput.value;
+    tasksList.innerHTML += `<li>${newTask}</li>`;
+
+
 
 
 })
 
+
+for (const taskObj of tasks) {
+    tasksList.innerHTML += `<li class="task__list">
+    <p class="each__task></li>;`}
 
 
 
